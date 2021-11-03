c7d8078a7143689275b6e23366206e620e1391f8
Timothy Karani <kartimothy@gmail.com>
2021-11-03 04:24:58 +0300

    done styling page

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Cadmium Creators'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -35,103 +33,115 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
+        child: Container(
+          color: Colors.white30,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(right: 0.0),
-                child: Text(
-                  'Sign in to Cadmium Creators',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w100,
+            children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 20.0,
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5.0),
-                child: Text(
-                  'Enter your credentials to continue',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w100,
-                    color: Colors.grey[500],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(right: 0.0),
+                        child: Text(
+                          'Sign in to Cadmium Creators',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w100,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          'Enter your credentials to continue',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Email Address'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password'),
+                          obscureText: true,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Checkbox(
+                              value: rememberMe,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  rememberMe = value!;
+                                });
+                              },
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(right: 0.0),
+                              child: Text('Remember me'),
+                            ),
+                            Expanded(child: Container()),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Forgot Password'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(
+                              double.infinity,
+                              45,
+                            ), // double.infinity is the width and 30 is the height
+                          ),
+                          onPressed: () {},
+                          child: const Text('Sign In'),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.only(right: 0.0),
+                              child: Text('Don\'t have an account?'),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Get started'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Email Address'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Password'),
-                  obscureText: true,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Checkbox(
-                      value: rememberMe,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          rememberMe = value!;
-                        });
-                      },
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(right: 0.0),
-                      child: Text('Remember me'),
-                    ),
-                    Expanded(child: Container()),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Forgot Password'),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(
-                      double.infinity,
-                      45,
-                    ), // double.infinity is the width and 30 is the height
-                  ),
-                  onPressed: () {},
-                  child: const Text('Sign In'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(right: 0.0),
-                      child: Text('Don\'t have an account?'),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Get started'),
-                    ),
-                  ],
                 ),
               ),
             ],
