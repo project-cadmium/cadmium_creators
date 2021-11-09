edf958ef09e450f74885622ec168a91495097f81
Timothy Karani <kartimothy@gmail.com>
2021-11-10 01:24:48 +0300

    Added login form

--- lib/login/view/login_form.dart
+++ lib/login/view/login_form.dart
@@ -1,14 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:cadmium_creators/login/login.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.blue,
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state.status.isSubmissionFailure) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              const SnackBar(content: Text('Authentication Failure')),
+            );
+        }
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+            elevation: 0.0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 20.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(right: 0.0),
+                    child: Text(
+                      'Cadmium Creators',
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      'Sign in to your account to continue',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        // color: Colors.grey[500],
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: _UsernameInput(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: _PasswordInput(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: _LoginButton(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text('Forgot Password'),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 1.0),
+                          child: Text(
+                            '|',
+                            style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text('Create Account'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _UsernameInput extends StatelessWidget {
+  const _UsernameInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      buildWhen: (previous, current) => previous.username != current.username,
+      builder: (context, state) {
+        return TextField(
+          key: const Key('loginForm_usernameInput_textField'),
+          onChanged: (value) =>
+              context.read<LoginBloc>().add(LoginUsernameChanged(value)),
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: 'Email Address',
+            // TODO: Change this to email
+            errorText: state.username.invalid ? 'invalid email' : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _PasswordInput extends StatelessWidget {
+  const _PasswordInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      buildWhen: (previous, current) => previous.password != current.password,
+      builder: (context, state) {
+        return TextField(
+          key: const Key('loginForm_passwordInput_textField'),
+          onChanged: (value) =>
+              context.read<LoginBloc>().add(LoginPasswordChanged(value)),
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: 'Password',
+            errorText: state.password.invalid ? 'invalid password' : null,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<LoginBloc, LoginState>(
+      buildWhen: (previous, current) => previous.status != current.status,
+      builder: (context, state) {
+        return state.status.isSubmissionInProgress
+            ? const CircularProgressIndicator()
+            : ElevatedButton(
+                key: const Key('loginForm_continue_raisedButton'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(
+                    double.infinity,
+                    40,
+                  ), // double.infinity is the width and 30 is the height
+                ),
+                onPressed: state.status.isValidated
+                    ? () {
+                        context.read<LoginBloc>().add(const LoginSubmitted());
+                      }
+                    : null,
+                child: const Text('Sign In'),
+              );
+      },
     );
   }
 }
