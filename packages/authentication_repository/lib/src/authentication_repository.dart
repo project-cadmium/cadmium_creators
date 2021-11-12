b8f34074fa7fac46b890034a0b25f544db697ad4
Timothy Karani <kartimothy@gmail.com>
2021-11-12 04:36:27 +0300

    Can get key

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 enum AuthenticationStatus { unknown, authenticated, unauthenticated }
 
@@ -14,6 +16,20 @@ class AuthenticationRepository {
 
   Future<void> logIn(
       {required String username, required String password}) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8000/api/v1/auth/login/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': 'aberry',
+        'password': 'password',
+      }),
+    );
+
+    print(response.statusCode);
+    print(response.body);
+
     await Future.delayed(
       const Duration(seconds: 3),
       () => _controller.add(AuthenticationStatus.authenticated),
