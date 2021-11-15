537ed9ed14e92cd11789a4b678bc4d04afa7bbf5
Timothy Karani <kartimothy@gmail.com>
2021-11-15 06:09:47 +0300

    Added crude drawer

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
+      drawer: const NavigationDrawer(),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -35,6 +36,67 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              // mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                CircleAvatar(
+                  radius: 25.0,
+                  backgroundColor: Colors.white,
+                  child: Text('AH'),
+                ),
+                Padding(padding: EdgeInsets.only(top: 10.0)),
+                Text(
+                  'Drawer Header',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'test@example.com',
+                  style: TextStyle(
+                    // fontSize: 20.0,
+                    // fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
