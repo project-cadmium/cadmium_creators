9300f96c77a745797d2c93cb77fb18625b413540
Timothy Karani <kartimothy@gmail.com>
2021-11-16 06:35:53 +0300

    Added post method

--- lib/pages/instructor/repository/instructor_repository/instructor_repository.dart
+++ lib/pages/instructor/repository/instructor_repository/instructor_repository.dart
@@ -1,1 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/models.dart';
 
+class InstructorRepository {
+  Instructor? _instructor;
+
+  Future<void> createInstructor(
+    int userId,
+    String biography,
+    String token,
+  ) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8000/api/v1/instructors/'),
+      headers: <String, String>{
+        HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: 'Token $token',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'user_id': userId,
+        'biography': biography,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      debugPrint('createInstructor success ${response.body}');
+    } else {
+      throw Exception("${response.statusCode} ${response.body} ");
+    }
+  }
+}
