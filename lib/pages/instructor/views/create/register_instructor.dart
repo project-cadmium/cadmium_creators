0a571b4eedaa748daf37f228b2781c9a67f0be98
Timothy Karani <kartimothy@gmail.com>
2021-11-16 04:38:42 +0300

    Added drawer to registration page

--- lib/pages/instructor/views/create/register_instructor.dart
+++ lib/pages/instructor/views/create/register_instructor.dart
@@ -1,4 +1,8 @@
+import 'package:cadmium_creators/components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+import 'package:cadmium_creators/authentication/authentication.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RegisterInstructor extends StatelessWidget {
   const RegisterInstructor({Key? key}) : super(key: key);
@@ -7,8 +11,18 @@ class RegisterInstructor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      final User user = context.select(
+        (AuthenticationBloc bloc) => bloc.state.user,
+      );
+      return _scaffold(context, user);
+    });
+  }
+
+  Widget _scaffold(BuildContext context, User user) {
     return Scaffold(
       appBar: AppBar(title: const Text('Register as Instructor')),
+      drawer: NavigationDrawer(user: user),
       body: Container(
         width: 100,
         height: 100,
