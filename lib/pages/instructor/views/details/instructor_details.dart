25b4203837c63ed524982cd34bd93dd6485ec87f
Timothy Karani <kartimothy@gmail.com>
2021-11-17 16:21:40 +0300

    Basic page layout done

--- lib/pages/instructor/views/details/instructor_details.dart
+++ lib/pages/instructor/views/details/instructor_details.dart
@@ -24,10 +24,56 @@ class InstructorDetails extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Instructor Details')),
       drawer: NavigationDrawer(user: user),
-      body: Container(
-        width: 100,
-        height: 100,
-        color: Colors.blue,
+      body: const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: _DetailsTable(),
+        ),
+      ),
+    );
+  }
+}
+
+class _DetailsTable extends StatelessWidget {
+  const _DetailsTable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 4, horizontal: 5),
+              child: Text(
+                "Biography",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(5, 6, 5, 10),
+              child: Text("Hello gentlemen"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('EDIT'),
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 5),
+                minimumSize: const Size(
+                  30,
+                  30,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
