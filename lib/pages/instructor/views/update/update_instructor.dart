a3e55292ebc3e412715ed92c2e38f13ad12da732
Timothy Karani <kartimothy@gmail.com>
2021-11-18 20:44:51 +0300

    Can load data

--- lib/pages/instructor/views/update/update_instructor.dart
+++ lib/pages/instructor/views/update/update_instructor.dart
@@ -1,4 +1,6 @@
 import 'package:cadmium_creators/authentication/authentication.dart';
+import 'package:cadmium_creators/pages/instructor/repository/repository.dart';
+import 'package:cadmium_creators/pages/instructor/views/details/bloc/get_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:user_repository/user_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,32 +16,60 @@ class UpdateInstructor extends StatelessWidget {
       final User user = context.select(
         (AuthenticationBloc bloc) => bloc.state.user,
       );
-      return _scaffold(user);
+      final String token = context.select(
+        (AuthenticationBloc bloc) => bloc.state.authKey.key,
+      );
+      return _scaffold(user, token);
     });
   }
 
-  Widget _scaffold(user) {
+  Widget _scaffold(user, token) {
     return Scaffold(
       appBar: AppBar(title: const Text('Update Instructor')),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
-              'Your biography',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-            Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-            _BiographyInput(),
-            Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-            _SubmitButton(),
-          ],
+        child: BlocProvider(
+          create: (context) {
+            return GetBloc(instructorRepository: InstructorRepository())
+              ..add(GetInstructorInitial(userId: user.id, token: token));
+          },
+          child: BlocBuilder<GetBloc, GetState>(
+            builder: (context, state) {
+              if (state.status == GetStatus.success) {
+                return const _UpdateInstructorForm();
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _UpdateInstructorForm extends StatelessWidget {
+  const _UpdateInstructorForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          'Your biography',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+        _BiographyInput(),
+        Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+        _SubmitButton(),
+      ],
     );
   }
 }
