aac3b82a81853086bee70560644adb01e78de145
Timothy Karani <kartimothy@gmail.com>
2021-11-20 06:55:39 +0300

    Added initial and successful events

--- lib/pages/courses/sub_pages/list/bloc/course_list_bloc.dart
+++ lib/pages/courses/sub_pages/list/bloc/course_list_bloc.dart
@@ -1,14 +1,37 @@
 import 'package:bloc/bloc.dart';
 import 'package:cadmium_creators/pages/courses/courses.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'course_list_event.dart';
 part 'course_list_state.dart';
 
 class CourseListBloc extends Bloc<CourseListEvent, CourseListState> {
-  CourseListBloc() : super(const CourseListState.unknown()) {
-    on<CourseListEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  CourseListBloc({required CourseRepository courseRepository})
+      : _courseRepository = courseRepository,
+        super(const CourseListState.unknown()) {
+    on<GetCourseListInitial>(_onInitialEvent);
+    on<GetCourseListSuccessful>(_onGettingSuccessful);
+  }
+
+  final CourseRepository _courseRepository;
+
+  void _onInitialEvent(
+      GetCourseListInitial event, Emitter<CourseListState> emit) async {
+    debugPrint("_onInitialEvent fired");
+    try {
+      final List<Course>? courses = await _courseRepository.getCourses(
+          instructorId: event.instructorId, token: event.token);
+      debugPrint("_onInitialEvent $courses");
+      add(GetCourseListSuccessful(courses!));
+    } catch (e) {
+      debugPrint("_onInitialEvent: ${e.toString()}");
+      add(const GetCourseListUnsuccessful());
+    }
+  }
+
+  void _onGettingSuccessful(
+      GetCourseListSuccessful event, Emitter<CourseListState> emit) {
+    emit(CourseListState.success(event.courses));
   }
 }
