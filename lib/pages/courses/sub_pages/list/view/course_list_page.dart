b37039858ac20ea1082aa322159355ad61b75c24
Timothy Karani <kartimothy@gmail.com>
2021-11-20 05:48:26 +0300

    Added drawer to courses

--- lib/pages/courses/sub_pages/list/view/course_list_page.dart
+++ lib/pages/courses/sub_pages/list/view/course_list_page.dart
@@ -1,4 +1,8 @@
+import 'package:cadmium_creators/authentication/authentication.dart';
+import 'package:cadmium_creators/components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
 class CourseListPage extends StatelessWidget {
   const CourseListPage({Key? key}) : super(key: key);
@@ -7,8 +11,18 @@ class CourseListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      final User user = context.select(
+        (AuthenticationBloc bloc) => bloc.state.user,
+      );
+      return _scaffold(user);
+    });
+  }
+
+  Widget _scaffold(User user) {
     return Scaffold(
       appBar: AppBar(title: const Text('Courses')),
+      drawer: NavigationDrawer(user: user),
       body: Container(
         width: 100,
         height: 100,
