a194fd96f3cdba56267c60c4d27bbb6f05a02c90
Timothy Karani <kartimothy@gmail.com>
2021-11-20 06:43:01 +0300

    Done with course list get status

--- lib/pages/courses/sub_pages/list/bloc/course_list_state.dart
+++ lib/pages/courses/sub_pages/list/bloc/course_list_state.dart
@@ -1,10 +1,27 @@
 part of 'course_list_bloc.dart';
 
-abstract class CourseListState extends Equatable {
-  const CourseListState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum CourseListGetStatus { unknown, success, error }
 
-class CourseListInitial extends CourseListState {}
+class CourseListState extends Equatable {
+  const CourseListState._({
+    this.status = CourseListGetStatus.unknown,
+    this.course = Course.empty,
+  });
+
+  final CourseListGetStatus status;
+  final Course course;
+
+  const CourseListState.unknown() : this._();
+
+  const CourseListState.success(Course course)
+      : this._(
+          status: CourseListGetStatus.success,
+          course: course,
+        );
+
+  // TODO: Respond to this in the UI
+  const CourseListState.error() : this._(status: CourseListGetStatus.error);
+
+  @override
+  List<Object> get props => [status, course];
+}
