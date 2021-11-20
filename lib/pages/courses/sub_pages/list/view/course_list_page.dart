61d02d0a86b2a9d912f77e8010f64cde138c72cf
Timothy Karani <kartimothy@gmail.com>
2021-11-20 07:02:54 +0300

    Loading instructor

--- lib/pages/courses/sub_pages/list/view/course_list_page.dart
+++ lib/pages/courses/sub_pages/list/view/course_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:cadmium_creators/authentication/authentication.dart';
 import 'package:cadmium_creators/components/components.dart';
+import 'package:cadmium_creators/pages/instructor/repository/repository.dart';
+import 'package:cadmium_creators/pages/instructor/views/details/bloc/get_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
@@ -15,18 +17,48 @@ class CourseListPage extends StatelessWidget {
       final User user = context.select(
         (AuthenticationBloc bloc) => bloc.state.user,
       );
-      return _scaffold(user);
+      final String token = context.select(
+        (AuthenticationBloc bloc) => bloc.state.authKey.key,
+      );
+      return _scaffold(user, token);
     });
   }
 
-  Widget _scaffold(User user) {
+  Widget _scaffold(User user, String token) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Courses')),
-      drawer: NavigationDrawer(user: user),
-      body: Container(
-        width: 100,
-        height: 100,
-        color: Colors.blue,
+        appBar: AppBar(title: const Text('Courses')),
+        drawer: NavigationDrawer(user: user),
+        body: _instructorBlocProvider(user, token));
+  }
+
+  Widget _instructorBlocProvider(User user, String token) {
+    return BlocProvider(
+      create: (context) {
+        return GetBloc(instructorRepository: InstructorRepository())
+          ..add(GetInstructorInitial(userId: user.id, token: token));
+      },
+      child: BlocBuilder<GetBloc, GetState>(
+        buildWhen: (previous, current) =>
+            previous.instructor != current.instructor,
+        builder: (context, state) {
+          if (state.status == GetStatus.success) {
+            return Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
