c4e92fcf282b2fc5fa72e5a1d52ea7e18df2ae08
Timothy Karani <kartimothy@gmail.com>
2021-11-20 20:54:23 +0300

    Added form events

--- lib/pages/courses/form_logic/bloc/course_form_event.dart
+++ lib/pages/courses/form_logic/bloc/course_form_event.dart
@@ -6,3 +6,43 @@ abstract class CourseFormEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class CourseFormNameChanged extends CourseFormEvent {
+  const CourseFormNameChanged(this.name);
+
+  final String name;
+
+  @override
+  List<Object> get props => [name];
+}
+
+class CourseFormDescriptionChanged extends CourseFormEvent {
+  const CourseFormDescriptionChanged(this.description);
+
+  final String description;
+
+  @override
+  List<Object> get props => [description];
+}
+
+class CourseFormCreateSubmitted extends CourseFormEvent {
+  const CourseFormCreateSubmitted(
+      {required this.courseId, required this.token});
+
+  final int courseId;
+  final String token;
+
+  @override
+  List<Object> get props => [courseId, token];
+}
+
+class CourseFormUpdateSubmitted extends CourseFormEvent {
+  const CourseFormUpdateSubmitted(
+      {required this.courseId, required this.token});
+
+  final int courseId;
+  final String token;
+
+  @override
+  List<Object> get props => [courseId, token];
+}
