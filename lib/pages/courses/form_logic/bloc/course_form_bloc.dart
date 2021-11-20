e5c823c2bf5d1334c0e6459071eaa00d46de828a
Timothy Karani <kartimothy@gmail.com>
2021-11-20 21:03:35 +0300

    Able to respond to onchange events

--- lib/pages/courses/form_logic/bloc/course_form_bloc.dart
+++ lib/pages/courses/form_logic/bloc/course_form_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:cadmium_creators/pages/courses/form_logic/form_logic.dart';
+import 'package:cadmium_creators/pages/courses/repository/repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:formz/formz.dart';
 
@@ -7,9 +8,30 @@ part 'course_form_event.dart';
 part 'course_form_state.dart';
 
 class CourseFormBloc extends Bloc<CourseFormEvent, CourseFormState> {
-  CourseFormBloc() : super(const CourseFormState()) {
-    on<CourseFormEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  CourseFormBloc({required CourseRepository courseRepository})
+      : _courseRepository = courseRepository,
+        super(const CourseFormState()) {
+    on<CourseFormNameChanged>(_onNameChanged);
+    on<CourseFormDescriptionChanged>(_onDescriptionChanged);
+  }
+
+  final CourseRepository _courseRepository;
+
+  void _onNameChanged(
+      CourseFormNameChanged event, Emitter<CourseFormState> emit) {
+    final name = CourseName.dirty(event.name);
+    emit(state.copyWith(
+      courseName: name,
+      status: Formz.validate([name]),
+    ));
+  }
+
+  void _onDescriptionChanged(
+      CourseFormDescriptionChanged event, Emitter<CourseFormState> emit) {
+    final description = CourseDescription.dirty(event.description);
+    emit(state.copyWith(
+      courseDescription: description,
+      status: Formz.validate([description]),
+    ));
   }
 }
