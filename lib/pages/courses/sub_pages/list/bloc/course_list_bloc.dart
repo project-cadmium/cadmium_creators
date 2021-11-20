deced8610a0ce78763d4602e7c89dfd7e614a51c
Timothy Karani <kartimothy@gmail.com>
2021-11-20 06:44:23 +0300

    Resolved imports

--- lib/pages/courses/sub_pages/list/bloc/course_list_bloc.dart
+++ lib/pages/courses/sub_pages/list/bloc/course_list_bloc.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:cadmium_creators/pages/courses/courses.dart';
 import 'package:equatable/equatable.dart';
 
 part 'course_list_event.dart';
 part 'course_list_state.dart';
 
 class CourseListBloc extends Bloc<CourseListEvent, CourseListState> {
-  CourseListBloc() : super(CourseListInitial()) {
+  CourseListBloc() : super(const CourseListState.unknown()) {
     on<CourseListEvent>((event, emit) {
       // TODO: implement event handler
     });
