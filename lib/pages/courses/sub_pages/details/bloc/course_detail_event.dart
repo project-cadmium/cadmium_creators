a157a8a5eb48fa23a74d151e60ce3f4f68314b85
Timothy Karani <kartimothy@gmail.com>
2021-11-20 18:15:43 +0300

    Added course detail get event

--- lib/pages/courses/sub_pages/details/bloc/course_detail_event.dart
+++ lib/pages/courses/sub_pages/details/bloc/course_detail_event.dart
@@ -6,3 +6,39 @@ abstract class CourseDetailEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class CourseDetailGetSuccessful extends CourseDetailEvent {
+  const CourseDetailGetSuccessful(this.course);
+
+  final Course course;
+
+  @override
+  List<Object> get props => [course];
+}
+
+class CourseDetailGetInitial extends CourseDetailEvent {
+  const CourseDetailGetInitial({required this.courseId, required this.token});
+
+  final int courseId;
+  final String token;
+
+  @override
+  List<Object> get props => [];
+}
+
+class CourseDetailGetRefresh extends CourseDetailEvent {
+  const CourseDetailGetRefresh({required this.courseId, required this.token});
+
+  final int courseId;
+  final String token;
+
+  @override
+  List<Object> get props => [];
+}
+
+class CourseDetailGetUnsuccessful extends CourseDetailEvent {
+  const CourseDetailGetUnsuccessful();
+
+  @override
+  List<Object> get props => [];
+}
