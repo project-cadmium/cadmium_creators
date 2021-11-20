a512023964d7d33feb7808ebb7ac1de8354f81c7
Timothy Karani <kartimothy@gmail.com>
2021-11-20 18:11:19 +0300

    Updated state

--- lib/pages/courses/sub_pages/details/bloc/course_detail_bloc.dart
+++ lib/pages/courses/sub_pages/details/bloc/course_detail_bloc.dart
@@ -1,11 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:cadmium_creators/pages/courses/repository/models/course.dart';
 import 'package:equatable/equatable.dart';
 
 part 'course_detail_event.dart';
 part 'course_detail_state.dart';
 
 class CourseDetailBloc extends Bloc<CourseDetailEvent, CourseDetailState> {
-  CourseDetailBloc() : super(CourseDetailInitial()) {
+  CourseDetailBloc() : super(const CourseDetailState.unknown()) {
     on<CourseDetailEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/pages/courses/sub_pages/details/bloc/course_detail_state.dart
+++ lib/pages/courses/sub_pages/details/bloc/course_detail_state.dart
@@ -1,10 +1,27 @@
 part of 'course_detail_bloc.dart';
 
-abstract class CourseDetailState extends Equatable {
-  const CourseDetailState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum CourseDetailGetStatus { unknown, success, error }
 
-class CourseDetailInitial extends CourseDetailState {}
+class CourseDetailState extends Equatable {
+  const CourseDetailState._({
+    this.status = CourseDetailGetStatus.unknown,
+    this.course = Course.empty,
+  });
+
+  final CourseDetailGetStatus status;
+  final Course course;
+
+  const CourseDetailState.unknown() : this._();
+
+  const CourseDetailState.success(Course course)
+      : this._(
+          status: CourseDetailGetStatus.success,
+          course: course,
+        );
+
+  // TODO: Respond to this in the UI
+  const CourseDetailState.error() : this._(status: CourseDetailGetStatus.error);
+
+  @override
+  List<Object> get props => [status, course];
+}
